3a153f20175ed9a5affe5dfe092a5b23040208af
Sone <sthaya@gmail.com>
2020-11-22 15:01:48 -0800

    created loader, used spinner gif

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from "react";
 import Map from "./components/Map";
+import Loader from "./components/Loader";
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      setLoading(true);
+
+      const res = await fetch(
+        "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+      );
+
+      const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
+    };
+
+    fetchEvent();
+
+    console.log(eventData);
+  }, []);
+
   return (
     <div>
-      <Map />
+      {!loading ?  <Map eventData={eventData} /> : <Loader />}
+     
     </div>
   );
 }
